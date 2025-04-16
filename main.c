ec991bfbcced58a8fe66d8279858a9a42d64a790
elias.ayeb@gmail.com <elias.ayeb@gmail.com>
2025-04-16 19:11:58 +0200

    test 1

--- main.c
+++ main.c
@@ -1,2 +1,28 @@
 //Write your code in this file
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
+int main (){
+    char name[5][50];
+    int results[5][13];
+    float averages[5];
+    float total_average = 0;
+
+    for (int a=0; a < 5; a++)
+        {
+        printf("Enter name and 13 test results for student %d:\n", a+1);
+        scanf("%50s", name[a]);
+
+
+        }
+
+
+
+
+
+
+
+
+    return 0;
+}
